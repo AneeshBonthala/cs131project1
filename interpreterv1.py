9238e9149da75c4034fa07f6e2483d0653d00e79
ticalceducation <aneeshbonthala15@gmail.com>
2023-10-20 15:44:50 -0700

    working version 1.0

--- interpreterv1.py
+++ interpreterv1.py
@@ -1,66 +1,83 @@
 from brewparse import parse_program
-from intbase import InterpreterBase
+from intbase import InterpreterBase, ErrorType
 
 class Interpreter(InterpreterBase):
+
 
     def __init__(self, console_output = True, inp = None, trace_output = False):
         super().__init__(console_output, inp)
         self.variables = {}
 
-    # for testing purposes
-    def print(self, program):
-        ast = parse_program(program)
-        return ast
 
     def eval_expr(self, expr):
+
         if expr.elem_type == 'int' or expr.elem_type == 'string':
             return expr.get('val')
-        elif expr.elem_type == 'var':
-            return self.variables[expr.get('name')]
-        elif expr.elem_type == '+':
-            return self.eval_expr(expr.get('op1')) + self.eval_expr(expr.get('op2'))
-        elif expr.elem_type == '-':
-            return self.eval_expr(expr.get('op1')) - self.eval_expr(expr.get('op2'))
-        elif expr.elem_type == 'fcall':
+        
+        if expr.elem_type == 'var':
+            var = expr.get('name')
+            if var == None: super().error(ErrorType.NAME_ERROR, 'Variable has not been defined.')
+            return self.variables[var]
+        
+        if expr.elem_type == '+':
+            try: result = self.eval_expr(expr.get('op1')) + self.eval_expr(expr.get('op2'))
+            except TypeError: super().error(ErrorType.TYPE_ERROR, 'Incompatible types for arithmetic operation.')
+            else: return result
+        
+        if expr.elem_type == '-':
+            try: result = self.eval_expr(expr.get('op1')) - self.eval_expr(expr.get('op2'))
+            except TypeError: super().error(ErrorType.TYPE_ERROR, 'Incompatible types for arithmetic operation.')
+            else: return result
+        
+        if expr.elem_type == 'fcall':
             return self.run_function(expr)
+        
 
     def run_assignment(self, statement):
         self.variables[statement.get('name')] = self.eval_expr(statement.get('expression'))
 
+
     def run_function(self, statement):
         args = statement.get('args')
+
         if statement.get('name') == 'inputi':
-            # handle incorrect # or type of args error
-            super().output(args[0])
-            return super().get_input()
-        elif statement.get('name') == 'print':
-            # handle incorrect # or type of args error
-            super().output(args[0])
+            if len(args) > 1: super().error(ErrorType.NAME_ERROR, 'Invalid number of parameters provided.')
+            if len(args) == 1: super().output(self.eval_expr(args[0]))
+            return int(super().get_input())
+        
+        if statement.get('name') == 'print':
+            result = ''
+            for arg in args:
+                eval_arg = self.eval_expr(arg)
+                result += str(eval_arg)
+            super().output(result)
+
 
     def run_statement(self, statement):
+
         if statement.elem_type == '=':
             self.run_assignment(statement)
+
         elif statement.elem_type == 'fcall':
             self.run_function(statement)
 
+
     def run(self, program):
         ast = parse_program(program)
-        main_node = ast.get('functions')[0]
+
+        main_node = None
+        for func in ast.get('functions'):
+            if func.get('name') == 'main': main_node = func
+        if not main_node: super().error(ErrorType.NAME_ERROR, "No main function was found.")
+
         for statement in main_node.get('statements'):
             self.run_statement(statement)
 
 
+# def test():
+#     inter = Interpreter()
+#     with open('test.txt') as file:
+#         prog = file.read()
+#     inter.run(prog)
 
-
-
-    
-def test():
-    inter = Interpreter()
-    with open('test.txt') as test:
-        prog = test.read()
-
-    with open('test.txt', 'a') as test:
-        test.write('\n')
-        test.write(str(inter.run(prog)))
-
-test()
+# test()
