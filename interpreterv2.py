58b574052153b271b05d63d7ee13d6586d5dbaf4
ticalceducation <aneeshbonthala15@gmail.com>
2023-10-27 20:53:10 -0700

    if else statements working

--- interpreterv2.py
+++ interpreterv2.py
@@ -56,9 +56,9 @@ class Interpreter(InterpreterBase):
 
             if elem_type == '+':
                 # It is illegal to use arithmetic operations on non-integer types, with the exception of using + to concatenate strings. Doing so must result in an error of ErrorType.TYPE_ERROR.
-                if op1type != 'int' or op1type != 'string':
+                if op1type != 'int' and op1type != 'string':
                     self.error_types(op1type, op2type)
-                if op2type != 'int' or op2type != 'string':
+                if op2type != 'int' and op2type != 'string':
                     self.error_types(op1type, op2type)
                 if op1type != op2type:
                     self.error_types(op1type, op2type)
@@ -69,6 +69,8 @@ class Interpreter(InterpreterBase):
                 dict = {
                     '-': lambda x, y: x - y,
                     '*': lambda x, y: x * y,
+                    # Division of integers must result in a truncated integer result, like using a // b with Python
+                    # You need not handle division by zero errors. Should such a division occur your interpreter can have undefined behavior, including behaviors that are different from our solution
                     '/': lambda x, y: x // y
                 }
                 return Element('int', val = dict[elem_type](op1val, op2val))
@@ -89,7 +91,9 @@ class Interpreter(InterpreterBase):
                         '<': lambda x, y: true() if x < y else false(),
                         '<=': lambda x, y: true() if x <= y else false(),
                         '>': lambda x, y: true() if x > y else false(),
-                        '>=': lambda x, y: true() if x >= y else false()
+                        '>=': lambda x, y: true() if x >= y else false(),
+                        '||': lambda x, y: true() if x or y else false(),
+                        '&&': lambda x, y: true() if x and y else false()
                     }
                     return dict[elem_type](op1val, op2val)
         
@@ -100,8 +104,10 @@ class Interpreter(InterpreterBase):
 
 
     def run_function(self, statement):
-        args = statement.get('args')
         name = statement.get('name')
+        args = statement.get('args')
+        statements = statement.get('statements')
+        
 
         if name == 'inputi':
             if len(args) > 1: self.error_args('inputi')
@@ -111,6 +117,15 @@ class Interpreter(InterpreterBase):
                 super().output(prompt.get('val'))
             #You may assume that only valid integers will be entered in response to an inputi() prompt and do NOT need to test for non-integer values being entered
             return Element('int', val = int(super().get_input()))
+        
+        # You must implement the inputs() function which inputs and returns a string as its return value. It must use our InterpreterBase.get_input() method to get input. It may take either one or no parameters
+        if name == 'inputs':
+            if len(args) > 1: self.error_args('inputs')
+            if len(args) == 1:
+                prompt = self.eval_expr(args[0])
+                if prompt.elem_type != 'string': self.error_args('inputs', 'type')
+                super().output(prompt.get('val'))
+            return Element('string', val = super().get_input())
         
         if name == 'print':
             result = ''
@@ -122,8 +137,46 @@ class Interpreter(InterpreterBase):
                 result += str(eval_arg)
                 # You will not be tested on printing nil values. In such scenarios, your interpreter may behave in any way you like, and may act in a different way than our solution.
             super().output(result)
+            # print() function can be called within expressions. The return value of print() should always be nil.
+            return Element('nil')
+        
+        else:
+            if (name, len(args)) not in self.variables:
+                self.error_not_found(name, 'function')
+            self.define_function(self.variables[(name, len(args))], args)
 
-        else: self.error_not_found(name, 'function')
+
+    # def define_function(self, func, args):
+    #     for statement in func.get('statements'):
+    #         self.run_statement(statement)
+        
+
+    def run_if(self, statement):
+        condition_true = self.eval_expr(statement.get('condition')).get('val')
+        else_statements = statement.get('else_statements')
+        before_len = len(self.variables.keys())
+        start_of_scope = None
+
+        if condition_true:
+            for s in statement.get('statements'):
+                self.run_statement(s)
+                if start_of_scope is None and before_len != len(self.variables.keys()):
+                    start_of_scope = len(self.variables.keys()) - 1
+        elif else_statements:
+            for s in else_statements:
+                self.run_statement(s)
+                if start_of_scope is None and before_len != len(self.variables.keys()):
+                    start_of_scope = len(self.variables.keys()) - 1
+        
+        self.garbage_collection(start_of_scope)
+
+        
+    def run_while(self, statement):
+        pass
+
+
+
+
 
 
     def run_statement(self, statement):
@@ -133,6 +186,10 @@ class Interpreter(InterpreterBase):
 
         elif elem_type == 'fcall': self.run_function(statement)
 
+        elif elem_type == 'if': self.run_if(statement)
+
+        elif elem_type == 'while': self.run_while(statement)
+
 
     def run(self, program):
         ast = parse_program(program)
@@ -140,10 +197,20 @@ class Interpreter(InterpreterBase):
         main_node = None
         for func in ast.get('functions'):
             if func.get('name') == 'main': main_node = func
+            # You may overload a function, defining multiple versions of it that take different numbers of parameters
+            else: self.variables[(func.get('name'), len(func.get('args')))] = func
         if not main_node: self.error_not_found('main', 'function')
 
         for statement in main_node.get('statements'):
             self.run_statement(statement)
+
+
+    def garbage_collection(self, start_of_scope):
+        if start_of_scope is None: return
+        for i in range(len(self.variables.keys())):
+            if i >= start_of_scope:
+                key = list(self.variables.keys())[i]
+                del self.variables[key]
 
 
 def test():
